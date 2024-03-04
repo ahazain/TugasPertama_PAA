2e9c6ea817287b6c5638e274d3fed396fd7a690c
ahazain <ahmadfariddd5@gmail.com>
2024-03-04 13:01:00 +0700

    first commit

---
+++ Project_API/ProjectAPI/models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectAPI.models
+{
+    public class Person
+    {
+        public int id_person { get; set; }
+        public string nama { get; set; }
+        public string alamat { get; set; }
+        public string email { get; set; }
+    }
+}
